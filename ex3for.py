e738151030f3f1963effb851968050d520391cf0
Bianca Tavares Carvalho <biancatavares.tiadscs@gmail.com>
2026-04-12 21:14:47 -0300

    Add files via upload

---
+++ ex3for.py
@@ -0,0 +1,5 @@
+soma=0
+n=int(input("digite um número positivo "))
+for i in range (1, n+1):
+ soma= soma +1/i
+print("a soma é ",soma)
---
+++ exercicio02 (2).py
@@ -0,0 +1,4 @@
+for i in range (1,52,):
+    print(i,  end= " ")
+for i in range (52,101,2):
+    print(i,  end=" ")
